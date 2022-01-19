2c6df5a42c5902e50b59f55526e3e284d580e8a7
Royam Jain <87959809+royamjj@users.noreply.github.com>
2022-01-19 15:04:01 +0530

    Update arduino_code.ino

--- arduino_code.ino
+++ arduino_code.ino
@@ -13,29 +13,29 @@ void setup() {
 }
 
 void loop() {
-while(Serial.available()==0){}
-status=Serial.readString();
-Serial.println(status);
-if (status == "0"){
-signal(0, green, buzzer, 1, 125, fan);
-}
-else if (status == "1"){
-  signal(1200, yellow, buzzer, 10, 200, fan);
-}
-else if (status == "2"){
-  signal(1200, yellow, buzzer, 10, 50, fan);
-}
-else if (status == "3"){
-  signal(5000, red, buzzer, 1, 255, fan);
-}
-else if (status == "4"){
-  signal(5000, red, buzzer, 1, 0, fan);
-}
-else if (status == "5"){
-  signal(200, red, buzzer, 10, 255, fan);
-}
-else if (status == "6"){
-  signal(200, red, buzzer, 10, 0, fan);
+  while(Serial.available()==0){}
+  status=Serial.readString();
+  Serial.println(status);
+  if (status == "0"){
+  signal(0, green, buzzer, 1, 125, fan);
+  }
+  else if (status == "1"){
+    signal(1200, yellow, buzzer, 10, 200, fan);
+  }
+  else if (status == "2"){
+    signal(1200, yellow, buzzer, 10, 50, fan);
+  }
+  else if (status == "3"){
+    signal(5000, red, buzzer, 1, 255, fan);
+  }
+  else if (status == "4"){
+    signal(5000, red, buzzer, 1, 0, fan);
+  }
+  else if (status == "5"){
+    signal(200, red, buzzer, 10, 255, fan);
+  }
+  else if (status == "6"){
+    signal(200, red, buzzer, 10, 0, fan);
 }
 void signal(int time_delay, int led_pin,int pin_buzzer,int times, int fan_speed, int fan_pin){
   analogWrite(fan_pin, fan_speed);
